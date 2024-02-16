e8035bd78424beeff7080c987c07578be50a1e4b
vj_rajawat <vijayrajawat01@gmail.com>
2024-02-16 19:17:30 +0530

    IntegerArrayMemoryAllo

---
+++ src/IntMemoryAllo.java
@@ -0,0 +1,10 @@
+public class IntMemoryAllo {
+    public static void main(String[] args) {
+        int[] arr = { 12, 23, 35, 41, 50 }; // Array store in Stack Memory
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
